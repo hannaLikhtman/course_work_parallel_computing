171bd7a4669a68b8e71bcd7a0526d9abedffdd91
Hanna Likhtman <hanna.likhtman@gmail.com>
2022-01-19 07:15:13 +0200

    added getters and setters Files.java

--- src/com/indexer/Files.java
+++ src/com/indexer/Files.java
@@ -4,6 +4,37 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class Files {
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    public String getFilepath() {
+        return filepath;
+    }
+
+    public void setFilepath(String filepath) {
+        this.filepath = filepath;
+    }
+
+    public int getVariant() {
+        return variant;
+    }
+
+    public void setVariant(int variant) {
+        this.variant = variant;
+    }
 
     //Constants used to bear with files
     private int num1 = 12500;
